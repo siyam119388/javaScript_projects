d259b8892190c6da65c0bf6abfcf74795b1de838
Md. Ashikur Rahman Siyam <149653672+siyam119388@users.noreply.github.com>
2024-10-11 21:42:01 +0600

    Update stopwatch.js

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,4 +1,3 @@
-
 let timerDisplay = document.querySelector('.timerDisplay');
 let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
@@ -15,3 +14,34 @@ startBtn.addEventListener('click', function(){
         clearInterval(timerId);
     }
     timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 00;
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+
+}
