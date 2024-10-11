ebb50461964c30e6691fbb0907e1f71fc600a601
Md. Ashikur Rahman Siyam <149653672+siyam119388@users.noreply.github.com>
2024-10-11 21:40:51 +0600

    Update stopwatch.js

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,1 +1,10 @@
 
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
