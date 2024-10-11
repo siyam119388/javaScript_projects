26098917dde68d4d897f0b3c6d6128fd1f1e7571
Md. Ashikur Rahman Siyam <149653672+siyam119388@users.noreply.github.com>
2024-10-11 21:41:32 +0600

    Update stopwatch.js

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -8,3 +8,10 @@ let msec = 00;
 let secs = 00;
 let mins = 00;
 
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
